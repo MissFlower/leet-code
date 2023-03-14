8a9d1997ca14416c035dd9267851da2b1896a4ab
DoveyLoveyCora <DoveyLoveyCora@163.com>
2023-03-14 21:32:56 +0800

    feat: 二叉树最大深度

---
+++ 104.二叉树的最大深度.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=javascript
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (root === null) {
+    return 0
+  }
+
+  const leftDep = maxDepth(root.left)
+  const rightDep = maxDepth(root.right)
+
+  return Math.max(leftDep, rightDep) + 1
+};
+// @lc code=end
+
